a280ae4ca471947abe6544932d14dfb1a8ddcfd0
Ramin Karimpor <ramin.kp81@gmail.com>
2024-01-12 19:49:36 +0330

    intialize app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+  return <div className="font-Dana-Bold">
+    
+    رامین کریم پور بناب
+  </div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Dana: "Dana",
+        "Dana-Medium": "Dana-Medium",
+        "Dana-Bold": "Dana-Bold",
+      },
+    },
+  },
+  plugins: [],
+};
